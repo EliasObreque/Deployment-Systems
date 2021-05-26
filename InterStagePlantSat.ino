c5a7b70e85fe518e744d25fb0ca2dabaaa43b3dd
EliasObreque <eliasobreque33@gmail.com>
2021-05-26 17:40:45 -0400

    i2c read

--- InterStagePlantSat.ino
+++ InterStagePlantSat.ino
@@ -17,6 +17,7 @@ products from Adafruit!
 #define READ_SW_FACE 33
 #define BURN_FACE 34
 #define SET_BURN 35
+#define SENSORS_TEMP_STATUS 36
 
 #define pin_sw_x_plus 8 
 #define pin_sw_x_minus 4 
@@ -30,12 +31,13 @@ products from Adafruit!
 
 const byte I2C_SLAVE_ADDR = 0x40;
 
-int cmd_num;
-int cmd_value;
+int cmd_num = 10;
+int cmd_value = 5;
 
-const int num_temp_sensor = 8;
+const int num_temp_sensor = 2;
 float current_temp[num_temp_sensor];
 int state_sw = 0;
+int flag_i2c_error_temp = 0;
 
 unsigned long current_time;
 int period_time = 3000; // milisecond
@@ -60,12 +62,25 @@ void check_i2c_sensor();
 void shutdown_wake_sensor();
 void get_resolution();
 void read_sw_state();
+union temp_sensor_Union  
+{
+      float temp_Float;
+      unsigned char temp_Bytes[4];
+};
 
 
 void setup() {
+
+  // debug usb
+  Serial.begin(9600);
+  
+  init_i2c_sensor();
+
+  wake_sensors();
   Wire.begin(I2C_SLAVE_ADDR);               
   Wire.onReceive(readMasterWrite);
   Wire.onRequest(responseToMasterRead);
+  
   pinMode(pin_sw_x_plus, INPUT);
   pinMode(pin_sw_y_minus, INPUT);
   pinMode(pin_sw_y_plus, INPUT);
@@ -76,12 +91,6 @@ void setup() {
   pinMode(pin_burn_y_plus, OUTPUT);
   pinMode(pin_burn_y_minus, OUTPUT);
   
-  Serial.begin(9600);
-  while (!Serial); //waits for serial terminal to be open, necessary in newer arduino boards.
-  // Make sure the sensor is found, you can also pass in a different i2c
-  // address with tempsensor.begin(0x19) for example, also can be left in blank for default address use
-  // Also there is a table with all addres possible for this sensor, you can connect multiple sensors
-  // to the same i2c bus, just configure each sensor with a different address and define multiple objects for that
   //  A2 A1 A0 address
   //  0  0  0   0x18  this is the default address
   //  0  0  1   0x19
@@ -91,20 +100,7 @@ void setup() {
   //  1  0  1   0x1D
   //  1  1  0   0x1E
   //  1  1  1   0x1F
-
-  //init_i2c_sensor();
-  //check_i2c_sensor();
- // sets the resolution mode of reading, the modes are defined in the table bellow:
-  
-  /*t1.setResolution(3);
-  t2.setResolution(3);
-  t3.setResolution(3);
-  t4.setResolution(3);
-  t5.setResolution(3);
-  t6.setResolution(3);
-  t7.setResolution(3);
-  t8.setResolution(3);
-  */
+    
   // Mode Resolution SampleTime
   //  0    0.5°C       30 ms
   //  1    0.25°C      65 ms
@@ -113,7 +109,7 @@ void setup() {
 }
 
 void loop() {
-  // wake up MCP9808 - power consumption ~200 mikro Ampere
+  //wake up MCP9808 - power consumption ~200 mikro Ampere
   //wake_sensors();
   //read_sensors();
   //print_data_sensors();
@@ -122,14 +118,12 @@ void loop() {
   //int face = 1;
   //activate_resistor(face);
   //Serial.println("waiting...");
-  //delay(100);
-  Serial.print(cmd_num); Serial.print("\t"); Serial.println(cmd_value);
+  delay(100);
 }
 
 void readMasterWrite(int howMany){
   cmd_num = Wire.read();
   cmd_value = Wire.read();    // receive byte as an integer
-
   
   if (cmd_num == START_SENSORS_TEMP){
     wake_sensors();
@@ -149,115 +143,163 @@ void readMasterWrite(int howMany){
   }
 
 void responseToMasterRead(){
-  if (cmd_num == GET_TEMP){
-    //for (int i=0; i < 7; i++){
-    //  Wire.write(current_temp[i]); Wire.write("\t");
-    //}
-    Wire.write((uint8_t*) current_temp, sizeof(current_temp)); 
+  if (cmd_num == START_SENSORS_TEMP){
+    if (flag_i2c_error_temp == 0){
+      Wire.write(flag_i2c_error_temp);
+    }
+    else {
+      Wire.write(flag_i2c_error_temp);
+    }
+  }
+  else if (cmd_num == GET_TEMP){
+    current_temp[0] = 123.1;
+    current_temp[1] = 11.1;
+    Wire.write((byte *) current_temp, sizeof(current_temp));
   }
   else if (cmd_num == READ_SW_FACE){
-    Wire.write(cmd_value);
-    Wire.write(state_sw);
+    Wire.write((int) cmd_value);
+    Wire.write((int) state_sw);
   }
+  else if (cmd_num == SENSORS_TEMP_STATUS){
+    Wire.write(flag_i2c_error_temp);
   }
+}
 
 void read_sw_state(int face){
   if (face == 1){
-  state_sw = digitalRead(pin_sw_x_plus);
-  //Serial.print(1);
-  //Serial.println(state);
+    state_sw = digitalRead(pin_sw_x_plus);
   }
   else if (face == 2){
-  state_sw = digitalRead(pin_sw_y_plus);
-  //Serial.print(2);
-  //Serial.println(state);
+    state_sw = digitalRead(pin_sw_y_plus);
   }
   else if (face == 3){
-  state_sw = digitalRead(pin_sw_x_minus);
-  //Serial.print(3);
-  //Serial.println(state);
+    state_sw = digitalRead(pin_sw_x_minus);
   }
   else if (face == 4){
-  state_sw = digitalRead(pin_sw_y_minus);
-  //Serial.print(4);
-  //Serial.println(state);
+    state_sw = digitalRead(pin_sw_y_minus);
   }
 }
 
 void activate_resistor(int face){
   int selected_pin;
+  bool active = false;
   if (face == 1){
-    selected_pin = pin_burn_x_plus;
+    if (digitalRead(pin_sw_x_plus) == 1){
+      selected_pin = pin_burn_x_plus;
+    }
   }
   else if (face == 3){
-    selected_pin = pin_burn_x_minus;
+    if (digitalRead(pin_sw_x_minus) == 1){
+      selected_pin = pin_burn_x_minus;
+    }
   }
   else if (face == 2){
-    selected_pin = pin_burn_y_plus;
+    if (digitalRead(pin_sw_y_plus) == 1){
+      selected_pin = pin_burn_y_plus;
+    }
   }
   else if (face == 4){
-    selected_pin = pin_burn_y_minus;
+    if (digitalRead(pin_sw_y_minus) == 1){
+      selected_pin = pin_burn_y_minus;
+    }
   }  
 
-  for (int i=0; i < attempt_burn; i++){
-    current_time = millis();
-    Serial.print("Attempt"); Serial.print("\t");
-    Serial.println(i);
-    while (millis() < current_time + period_time){
-      digitalWrite(selected_pin, HIGH);
+  if (active) {
+    for (int i=0; i < attempt_burn; i++){
+      current_time = millis();
+      Serial.print("Attempt"); Serial.print("\t");
+      Serial.println(i);
+      while (millis() < current_time + period_time){
+        digitalWrite(selected_pin, HIGH);
+      }
+      digitalWrite(selected_pin, LOW);
+      delay(int (period_time / 2));
     }
-    digitalWrite(selected_pin, LOW);
-    delay(int (period_time / 2));
   }
 }
 
 void print_data_sensors(){
-  for (int i=0; i < 7; i++){
+  for (int i=0; i < 2; i++){
     Serial.print(current_temp[i], 4); Serial.print("\t");
     }
-  Serial.print(current_temp[7], 4); Serial.println("\t"); 
+  //Serial.print(current_temp[7], 4); Serial.println("\t"); 
 }
 
-void read_sensors(){
-  current_temp[0] = t1.readTempC();
-  current_temp[1] = t2.readTempC();
-  current_temp[2] = t3.readTempC();
-  current_temp[3] = t4.readTempC();
-  current_temp[4] = t5.readTempC();
-  current_temp[5] = t6.readTempC();
-  current_temp[6] = t7.readTempC();
-  current_temp[7] = t8.readTempC();  
-  }
+void read_sensors(){    
+  current_temp[0] = (int)(t1.readTempC()*1000);
+  current_temp[1] = (int)(t2.readTempC()*1000);
+  /*current_temp[2] = (int)(t3.readTempC()*1000);
+  current_temp[3] = (int)(t4.readTempC()*1000);
+  current_temp[4] = (int)(t5.readTempC()*1000);
+  current_temp[5] = (int)(t6.readTempC()*1000);
+  current_temp[6] = (int)(t7.readTempC()*1000);
+  current_temp[7] = (int)(t8.readTempC()*1000);  
+  */}
 
 void wake_sensors(){
-  check_i2c_sensor();
-  t1.wake();
-  t2.wake();
-  t3.wake();
-  t4.wake();
-  t5.wake();
-  t6.wake();
-  t7.wake();
-  t8.wake();
-  }
+  if (flag_i2c_error_temp == 0){
+    t1.wake();
+    t2.wake();
+    /*t3.wake();
+    t4.wake();
+    t5.wake();
+    t6.wake();
+    t7.wake();
+    t8.wake();
+  */}
+}
 
 void shutdown_wake_sensor(){
   t1.shutdown_wake(1);
   t2.shutdown_wake(1);
-  t3.shutdown_wake(1);
+  /*t3.shutdown_wake(1);
   t4.shutdown_wake(1);
   t5.shutdown_wake(1);
   t6.shutdown_wake(1);
   t7.shutdown_wake(1);
   t8.shutdown_wake(1);
-  }
+  */}
 
 void get_resolution(){
   Serial.println (t1.getResolution());
   }
 
 void init_i2c_sensor(){
-  check_i2c_sensor();
+    if (!t1.begin(0x18)) {
+      flag_i2c_error_temp = 1;
+    }
+    if (!t2.begin(0x19)) {
+      flag_i2c_error_temp = 2;
+    }
+    /*
+    if (!t3.begin(0x1A)) {
+      flag_i2c_error_temp = 3;
+    }
+    if (!t4.begin(0x1B)) {
+      flag_i2c_error_temp = 4;
+    }
+    if (!t5.begin(0x1C)) {
+      flag_i2c_error_temp = 5;
+    }
+    if (!t6.begin(0x1D)) {
+      flag_i2c_error_temp = 6;
+    }
+  if (!t7.begin(0x1E)) {
+      flag_i2c_error_temp = 7;
+    }
+    if (!t8.begin(0x1F)) {
+      flag_i2c_error_temp = 8;
+    }
+    */
+      t1.setResolution(3);
+  t2.setResolution(3);
+  /*t3.setResolution(3);
+  t4.setResolution(3);
+  t5.setResolution(3);
+  t6.setResolution(3);
+  t7.setResolution(3);
+  t8.setResolution(3);
+  */
   }
 
 void check_i2c_sensor(){
@@ -286,3 +328,5 @@ void check_i2c_sensor(){
       Serial.print("8 "); Serial.println("1");
     }
   }
+
+
--- MasterTEST/MasterTEST.ino
+++ MasterTEST/MasterTEST.ino
@@ -6,6 +6,8 @@
 #define READ_SW_FACE 33
 #define BURN_FACE 34
 #define SET_BURN 35
+#define SENSORS_TEMP_STATUS 36
+
 
 const byte I2C_SLAVE_ADDR = 0x40;
 String cmd_name;
@@ -24,7 +26,7 @@ void loop() {
  	delay(4);
  	if (Serial.available() > 0) {
  		char c = Serial.read();  //gets one byte from serial buffer
-	    if (c == ' '){
+	    if (c == ' ' || c == '\n'){
 	    	cmd_change = 1;
 	    }
 	    else {
@@ -37,33 +39,41 @@ void loop() {
    		}
 	}
 }
-   if (cmd_name.length() > 0) {
-		Serial.print(cmd_name);
-		Serial.print(cmd_value);
-		sendCom(cmd_name, cmd_value);
-		reset_i2c_cmd();
+  if (cmd_name.length() > 0) {
+    Serial.print(cmd_name); Serial.print("\t"); Serial.println(cmd_value);
+    sendCom(cmd_name, cmd_value);
+    reset_i2c_cmd();
 	}
+    delay(500);
 }
 
 void reset_i2c_cmd(){
 	cmd_name = "";
-   	cmd_value = "";
-   	cmd_change = 0;
+  cmd_value = "";
+  cmd_change = 0;
 
 }
 
 void sendCom(String data, String value){
 	// Convert from String Object to String.
-	int selected_cmd;
+	int selected_cmd = 0;
 	int selected_value = 0; // {1: x+, 2: x-, 3: y+, 4 y-}
 
-	if (data == "is2_init_sensor"){
-		Serial.println(START_SENSORS_TEMP);
-		selected_cmd = START_SENSORS_TEMP;}
+	if (data == "is2_start_sensors"){
+		Serial.println("cmd start");
+		selected_cmd = START_SENSORS_TEMP;
+    	selected_value = 0;
+    }
+    else if (data == "is2_stop_sensors"){
+    	Serial.println("cmd stop");
+		selected_cmd = STOP_SENSORS_TEMP;
+    	selected_value = 0;	
+   	}
 	else if (data == "is2_get_state_panel") {
+  		Serial.print("cmd read sw");
 		selected_cmd = READ_SW_FACE;
 		selected_value = int(value[0]) - 48;
-   Serial.print(selected_cmd); Serial.print("\t"); Serial.println(selected_value);
+		Serial.println(selected_value);
 	}
 	else if (data == "is2_deploy_panel"){
 		selected_cmd = BURN_FACE;
@@ -71,33 +81,73 @@ void sendCom(String data, String value){
 	}
 	else if (data == "is2_set_deploy"){
 		selected_cmd = SET_BURN;
+    	selected_value = 0;
 	}
 	else if (data == "is2_get_temp"){
-	selected_cmd = GET_TEMP;
+  		Serial.println("cmd get temp");
+		selected_cmd = GET_TEMP;
+  		selected_value = 0;
+	}
+	else if (data == "is2_get_sensors_status"){
+		Serial.println("get sensors status");
+		selected_cmd = SENSORS_TEMP_STATUS;
+  		selected_value = 0;
 	}
 	
-	  Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device #40
-  	Wire.write(selected_cmd);        // sends five bytes
-  	Wire.write(selected_value);              // sends one byte  
+	Wire.beginTransmission(I2C_SLAVE_ADDR); // transmit to device #40
+  	Wire.write((int) selected_cmd);        // sends five bytes
+  	Wire.write((int) selected_value);              // sends one byte  
   	Wire.endTransmission();    // stop transmitting
 
-  if (selected_cmd == READ_SW_FACE){
-    Wire.requestFrom(I2C_SLAVE_ADDR, 2);
-    int face = Wire.read();
-    int state = Wire.read();
-    Serial.print(face); Serial.print("\t"); Serial.println(state);
-    } 
-   else if (selected_cmd == GET_TEMP){
-    const int VEC_MAX = 8;
-    float vec[VEC_MAX];
-    uint8_t* vp = (uint8_t*) vec;
-    Wire.requestFrom(I2C_SLAVE_ADDR, 8);
-    while (Wire.available()) {
-      *vp++ = Wire.read();
+   request_to_slave(selected_cmd);
+}
+
+void request_to_slave(int selected_cmd){
+	if (selected_cmd == READ_SW_FACE){
+		Wire.requestFrom(I2C_SLAVE_ADDR, 2);
+	    int face = Wire.read();
+	    int state = Wire.read();
+	    Serial.println("read sw");
+	    Serial.print((int) face); Serial.print("\t"); Serial.println(state);
+  	}
+  	else if (selected_cmd == START_SENSORS_TEMP){
+		int state = Wire.read();
+		Serial.println("start temp");
+	Serial.println(state);
+   	} 
+   	else if (selected_cmd == GET_TEMP){
+	    const int VEC_MAX = 2;
+	    float vec[VEC_MAX];
+	 
+	    Wire.requestFrom(I2C_SLAVE_ADDR, VEC_MAX * sizeof(float));
+
+    	for (int i = 0; i < VEC_MAX; i++){
+    		byte buff[VEC_MAX * sizeof(float)];
+			buff[0] = Wire.read();
+			buff[1] = Wire.read();
+			buff[2] = Wire.read();
+			buff[3] = Wire.read();
+
+			vec[i] = *((float*)(buff));
+    		Serial.println(vec[i]);
+    	}
+    	
+    	//for (int i = 0; i < VEC_MAX; i++) {
+  		//	Serial.println(vec_char[i]);
+    	//}
+	}
+    else if (selected_cmd == SENSORS_TEMP_STATUS){
+    	Wire.requestFrom(I2C_SLAVE_ADDR, 1);
+    	int status = Wire.read();
+    	Serial.println("status of sensors");
+    	Serial.println(status);
     }
-    for (int i = 0; i < VEC_MAX; i++) {
-      Serial.println(vec[i]);
-    }
-   }
-  delay(500);
+}
+
+float getFloat(byte packet[], int i)
+{
+    float out;
+    memcpy(&out, &packet[i], sizeof(float));
+
+    return out;
 }
